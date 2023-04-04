8043682778a2dcaa409c0cab2bf4056219a2fde1
Yongseok <yongseok.dev@gmail.com>
2023-04-04 11:07:39 +0900

    todo 10.2.2

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,7 +1,12 @@
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
-  return <TodoTemplate>Todo App을 만들기</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert></TodoInsert>
+    </TodoTemplate>
+  );
 }
 
 export default App;
