9a0ccb46c83fe9461064185c0867b79d67e38936
Yongseok <yongseok.dev@gmail.com>
2025-01-26 21:03:54 +0900

    vue study

---
+++ Vue3/one_book/3_Basics/hello-workd-vite/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="test">오 리액트와 다른 점.</div>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
+<!--  style scoped 속성을 사용하면 component내에서만 적용됨. -->
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
