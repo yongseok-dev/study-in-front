ed0262678fabd164c52bd64faf8857724cf19c80
Yongseok <yongseok.dev@gmail.com>
2025-01-27 10:57:13 +0900

    vue3+webpack setting

---
+++ Vue3/games/3_baseball/src/App.vue
@@ -0,0 +1,19 @@
+<!-- src/App.vue -->
+<template>
+  <div>
+    <h1>Hello, Vue 3 + Webpack!</h1>
+    <p>프로젝트가 성공적으로 빌드되었습니다.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
