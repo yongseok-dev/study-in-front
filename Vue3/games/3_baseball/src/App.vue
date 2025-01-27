0da40152935690f1ee7677e5dc5b153b7e51a060
Yongseok <yongseok.dev@gmail.com>
2025-01-27 12:25:47 +0900

    야구게임임

--- Vue3/games/3_baseball/src/App.vue
+++ Vue3/games/3_baseball/src/App.vue
@@ -1,14 +1,72 @@
 <!-- src/App.vue -->
 <template>
   <div>
-    <h1>Hello, Vue 3 + Webpack!</h1>
-    <p>프로젝트가 성공적으로 빌드되었습니다.</p>
+    <h1>숫자야구 게임</h1>
+    <form @submit="onSubmitForm">
+      <input type="text" v-model="input" />
+      <button>입력</button>
+    </form>
+    <p>시도: {{ msg.length }}</p>
+    <ul>
+      <li v-for="msg in msg">{{ msg }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      value: [
+        Math.ceil(Math.random() * 9),
+        Math.ceil(Math.random() * 9),
+        Math.ceil(Math.random() * 9),
+        Math.ceil(Math.random() * 9),
+      ],
+      input: undefined,
+      msg: [],
+    };
+  },
+  methods: {
+    onSubmitForm(e) {
+      console.log(this.result);
+      e.preventDefault();
+      let num1 = 0;
+      let num2 = 0;
+      const preInput = this.input;
+      this.input = undefined;
+      let tempValue = this.value.map((e) => e);
+      let tempInput = String(preInput).split("");
+
+      for (let i = 0; i < 4; i++) {
+        if (tempValue[i] == tempInput[i]) {
+          console.log("스트라이크" + tempValue[i]);
+          num1++;
+          tempValue[i] = undefined;
+          tempInput[i] = undefined;
+        }
+      }
+      for (let i = 0; i < 4; i++) {
+        if (tempInput[i] != undefined) {
+          for (let j = 0; j < 4; j++) {
+            if (tempInput[i] == tempValue[j]) {
+              console.log("볼" + tempValue[j]);
+              num2++;
+              tempValue[j] = undefined;
+              break;
+            }
+          }
+        }
+      }
+
+      if (num1 == 4) {
+        this.msg.push(`${preInput}: 홈런 ~~ !!`);
+      } else {
+        this.msg.push(`${preInput}: ${num1} 스트라이크, ${num2} 볼입니다.`);
+      }
+    },
+  },
 };
 </script>
 
